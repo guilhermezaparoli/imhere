b3ed35024d09f1cda1f358f2f719ae0b517e2dfd
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2024-10-06 11:28:18 -0300

    Adding Participant component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,31 @@
-import { Text, TextInput, View } from 'react-native';
-import {styles} from "./styles"
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { styles } from './styles';
+import { Participant } from "../../components/Participant";
 
 export function Home() {
+
+  function handleParticipantAdd(){
+    console.log("aqe")
+  }
   return (
-
     <View style={styles.container}>
-      <Text style={styles.text}>testeee</Text>
-      <TextInput placeholder="Nome do participante" placeholderTextColor="#6b6b6b"  style={styles.input}/>
-    </View>
+      <Text style={styles.text}>Nome do evento</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
+      <View style={styles.form}>
 
+      <TextInput
+        placeholder="Nome do participante"
+        placeholderTextColor="#6b6b6b"
+        style={styles.input}
+        />
+      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
+        </View>
+
+        <Participant name="teste"/>
+        <Participant name="teste2"/>
+        <Participant name="teste3"/>
+    </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -4,11 +4,13 @@ export const styles = StyleSheet.create({
     container: {
       backgroundColor: '#131016',
       flex: 1,
-      padding: 24
+      padding: 24,
+      marginTop: 48 
     },
     text: {
       color: '#fff',
-      fontWeight: 'bold'
+      fontWeight: 'bold',
+      fontSize: 18
     },
     input: {
         height: 56,
@@ -16,6 +18,30 @@ export const styles = StyleSheet.create({
         borderRadius: 5,
         color: "#fdfcfe",
         padding: 10,
-        fontSize: 16
+        fontSize: 16,
+        flex: 1,
+        marginRight: 12
+    }, 
+    buttonText: {
+      color: "#FFF",
+      fontSize: 24
+    },
+    button: {
+      width: 56,
+      height: 56,
+      borderRadius: 5,
+      backgroundColor: '#31CF67',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    eventDate: {
+      color: "#6B6B6B"
+    },
+    form: {
+      width: '100%',
+      flexDirection: 'row',
+      marginTop: 36,
+      marginBottom: 42
     }
+    
   });
